2a1c3627f3550ec38862c75a54cb4e71062669c4
YunusEmreNalbant <nalbantyunusemre@hotmail.com>
2019-06-07 16:09:44 +0300

    TodoList #6

--- todo.js
+++ todo.js
@@ -26,8 +26,25 @@ function deleteTodo(e) {
 
     if(e.target.className==="fa fa-remove"){
         e.target.parentElement.parentElement.remove();
+        deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
+
         showAlert('success','Todo başarı ile silindi!')
     }
+
+}
+
+function deleteTodoFromStorage(deletetodo) {
+   let todos = getTodosFromStorage();
+   todos.forEach(function (todo,index) {
+       if(todo === deletetodo){
+            todos.splice(index,1); // arrayden değeri silme
+       }
+   });
+
+   localStorage.setItem("todos",JSON.stringify(todos));
+
+
+
 
 }
 function loadAllTodosToUI() {
